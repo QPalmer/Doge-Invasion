d8845d2a65d8b0577ae7c525d2ebdff4cd170d3a
Elastic <QPalmer>
2021-08-01 15:36:04 -0700

    add update method to ship

--- ship.py
+++ ship.py
@@ -8,12 +8,23 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        #Load the ship image and get its rect.
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('assets/images/doge.png')
         self.rect = self.image.get_rect()
 
-        #Start each new ship at the bottom center of the screen. 
+        # Start each new ship at the bottom center of the screen. 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement Flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1 
 
     def drawship(self):
         """Draw the ship at its current location."""
