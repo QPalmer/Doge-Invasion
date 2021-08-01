3cab8739d373d206b62593915aab7031771418d0
Elastic <QPalmer>
2021-08-01 15:36:51 -0700

    add keyup / keydown events

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             
     
@@ -35,6 +36,22 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            # Move Ship on KEYDOWN    
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            
+            # Stop Ship on KEYUP
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
